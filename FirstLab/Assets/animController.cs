2eae41284312fa70ce820e5fccbcccc6847eb2b4
liza-i <45426013+liza-i@users.noreply.github.com>
2020-10-07 22:11:11 +0300

    Update animController.cs

--- FirstLab/Assets/animController.cs
+++ FirstLab/Assets/animController.cs
@@ -8,20 +8,20 @@ public class animController : MonoBehaviour
     public Transform Player;
     [SerializeField] private Animator anim;
     public float distance;
-    // Start is called before the first frame update
+    
     void Start()
     {
         anim = GetComponent<Animator>();
         Player = GameObject.FindWithTag("Player").transform;
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         distance = Vector3.Distance(Camera.main.transform.position, Player.transform.position);
         if ( distance<17 && distance>=10)
 
-        //if(Input.GetKeyDown("1"))
+    
         {
             anim.Play("Attack");
         }
